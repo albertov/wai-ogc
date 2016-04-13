cfea51e83016355c047b8a28bd33a5ccd177f022
Alberto Valverde <alberto@meteogrid.com>
2016-04-13 22:15:07 +0200

    implementing GetMap parser

--- spec/Network/Wai/OgcSpec.hs
+++ spec/Network/Wai/OgcSpec.hs
@@ -3,7 +3,8 @@ module Network.Wai.OgcSpec (main, spec) where
 
 import           Network.Wai.Ogc
 
-import           Data.String (fromString)
+import           Data.Monoid ((<>))
+import           Data.String (IsString, fromString)
 import           Network.Wai (Application, responseLBS)
 import           Network.HTTP.Types (status200, status400)
 import           Test.Hspec
@@ -32,39 +33,114 @@ spec = with (return app) $ do
 commonSpec :: SpecWith Application
 commonSpec = describe "common" $ do
   "/" `getFailsWith` MissingParameterError "SERVICE"
-  "/?SERVICE" `getFailsWith` UnknownServiceError ""
-  "/?SERVICE=" `getFailsWith` UnknownServiceError ""
-  "/?SeRViCe" `getFailsWith` UnknownServiceError ""
-  "/?SeRViCe=foo" `getFailsWith` UnknownServiceError "foo"
+  "/?SERVICE" `getFailsWith` EmptyParameterError "SERVICE"
+  "/?SERVICE=" `getFailsWith` EmptyParameterError "SERVICE"
+  "/?SeRViCe" `getFailsWith` EmptyParameterError "SERVICE"
+  "/?SeRViCe=foo" `getFailsWith` InvalidParameterError "SERVICE" "foo"
+
+wmsUrl :: (IsString a, Monoid a) => a -> a
+wmsUrl = ("/?SERVICE=WMS&" <>)
 
 wmsSpec :: SpecWith Application
 wmsSpec = describe "WMS" $ do
   describe "missing mandatory parameters" $ do
     "/?SERVICE=WMS" `getFailsWith` MissingParameterError "REQUEST"
-  describe "GetCapabilities" $ do
-    describe "with no optional parameters" $ do
-      "/?SERVICE=WMS&REQUEST=GetCapabilities"
-        `getSucceedsWith` wmsCapabilitiesRequest
-    describe "with VERSION parameter" $ do
-      "/?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.1.1"
-        `getSucceedsWith` wmsCapabilitiesRequest { version = Just Wms111 }
-      "/?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0"
-        `getSucceedsWith` wmsCapabilitiesRequest { version = Just Wms130 }
-      "/?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.0.0"
-        `getFailsWith` UnknownVersionError WMS "1.0.0"
-      "/?SERVICE=WMS&REQUEST=GetCapabilities&VERSION"
-        `getFailsWith` UnknownVersionError WMS ""
-    describe "with FORMAT parameter" $ do
-      "/?SERVICE=WMS&REQUEST=GetCapabilities&FORMAT=text/xml"
-        `getSucceedsWith`
-        wmsCapabilitiesRequest { format = Just (Type (Text "xml") []) }
-      "/?SERVICE=WMS&REQUEST=GetCapabilities&FORMAT=text--"
-        `getFailsWith` InvalidMimeError "text--"
-    describe "with UPDATESEQUENCE parameter" $ do
-      "/?SERVICE=WMS&REQUEST=GetCapabilities&UPDATESEQUENCE=34"
-        `getSucceedsWith` wmsCapabilitiesRequest { updateSequence = Just "34" }
-      "/?SERVICE=WMS&REQUEST=GetCapabilities&UPDATESEQUENCE"
-        `getFailsWith` EmptyUpdateSequenceError
+  wmsCapabilitiesSpec
+  wmsMapSpec
+
+wmsCapabilitiesSpec :: SpecWith Application
+wmsCapabilitiesSpec = describe "GetCapabilities" $ do
+  describe "with no optional parameters" $ do
+    wmsUrl "REQUEST=GetCapabilities"
+      `getSucceedsWith` wmsCapabilitiesRequest
+  describe "legacy 1.0.0 request without explicit version" $ do
+    wmsUrl "REQUEST=capabilities"
+      `getSucceedsWith` wmsCapabilitiesRequest
+  describe "with extra unknown query parameters" $ do
+    wmsUrl "REQUEST=GetCapabilities&USER=foo"
+      `getSucceedsWith` wmsCapabilitiesRequest
+  describe "with VERSION parameter" $ do
+    wmsUrl "REQUEST=GetCapabilities&VERSION=1.3.0"
+      `getSucceedsWith` wmsCapabilitiesRequest { wmsCapVersion = Just Wms130 }
+    wmsUrl "REQUEST=GetCapabilities&VERSION=1.3"
+      `getSucceedsWith` wmsCapabilitiesRequest { wmsCapVersion = Just Wms130 }
+    wmsUrl "REQUEST=GetCapabilities&VERSION=1.1.1"
+      `getSucceedsWith` wmsCapabilitiesRequest { wmsCapVersion = Just Wms111 }
+    wmsUrl "REQUEST=GetCapabilities&VERSION"
+      `getFailsWith` EmptyParameterError "VERSION"
+    describe "version 1.1.1 and 1.3.0 should send \"GetCapabilities\"\
+      \ instead of \"capabilites\"" $ do
+      wmsUrl "REQUEST=capabilities&VERSION=1.3.0"
+        `getFailsWith` InvalidParameterError "REQUEST" "capabilities"
+      wmsUrl "REQUEST=capabilities&VERSION=1.1.1"
+        `getFailsWith` InvalidParameterError "REQUEST" "capabilities"
+  describe "with legacy WMSTVER parameter" $ do
+    wmsUrl "REQUEST=GetCapabilities&WMSTVER=1.0.0"
+      `getFailsWith` InvalidParameterError "REQUEST" "GetCapabilities"
+    wmsUrl "REQUEST=capabilities&WMSTVER=1.0.0"
+      `getSucceedsWith` wmsCapabilitiesRequest { wmsCapVersion = Just Wms100 }
+    wmsUrl "REQUEST=capabilities&WMSTVER=1.0"
+      `getSucceedsWith` wmsCapabilitiesRequest { wmsCapVersion = Just Wms100 }
+    wmsUrl "REQUEST=capabilities&WMSTVER=1"
+      `getSucceedsWith` wmsCapabilitiesRequest { wmsCapVersion = Just Wms100 }
+    describe "version 1 should send \"capabilites\"\
+      \ instead of \"GetCapabilities\"" $ do
+      wmsUrl "REQUEST=GetCapabilities&WMSTVER=1.0.0"
+        `getFailsWith` InvalidParameterError "REQUEST" "GetCapabilities"
+  describe "with FORMAT parameter" $ do
+    wmsUrl "REQUEST=GetCapabilities&FORMAT=text/xml"
+      `getSucceedsWith`
+      wmsCapabilitiesRequest { wmsCapFormat = Just (Type (Text "xml") []) }
+    wmsUrl "REQUEST=GetCapabilities&FORMAT=text--"
+      `getFailsWith` InvalidParameterError "FORMAT" "text--"
+  describe "with UPDATESEQUENCE parameter" $ do
+    wmsUrl "REQUEST=GetCapabilities&UPDATESEQUENCE=34"
+      `getSucceedsWith`
+      wmsCapabilitiesRequest { wmsCapUpdateSequence = Just "34" }
+    wmsUrl "REQUEST=GetCapabilities&UPDATESEQUENCE"
+      `getFailsWith` EmptyParameterError "UPDATESEQUENCE"
+
+wmsMapSpec :: SpecWith Application
+wmsMapSpec = describe "GetMap" $ do
+  let getMap    = wmsUrl . ("REQUEST=GetMap&" <>)
+      getMap100 = wmsUrl . ("REQUEST=map&WMSTVER=1.0.0&" <>)
+      getMap111 = getMap . ("VERSION=1.1.1&" <>)
+      getMap130 = getMap . ("VERSION=1.3.0&" <>)
+      epsg23030 = maybe (error "should not happen") id (epsgCrs 23030)
+      pngFormat = Type (Image "png") []
+      mapReq    = wmsMapRequest
+        Wms130 [("foo","foo")] epsg23030 (Bbox 0 0 1 1) 1 2 pngFormat
+      getMap130Min
+        = getMap130
+        . ("LAYERS=foo&STYLES=foo&CRS=EPSG:23030&BBOX=0,0,1,1\
+           \&WIDTH=1&HEIGHT=2&FORMAT=image/png&" <>)
+
+  describe "with missing mandatory parameters" $ do
+    wmsUrl "REQUEST=GetMap" `getFailsWith` MissingParameterError "VERSION"
+    getMap130 "" `getFailsWith` MissingParameterError "LAYERS"
+    getMap130 "STYLES=foo" `getFailsWith` MissingParameterError "LAYERS"
+    getMap130 "LAYERS=foo" `getFailsWith` MissingParameterError "STYLES"
+    getMap130 "LAYERS=foo&STYLES=foo" `getFailsWith` MissingParameterError "CRS"
+    getMap111 "LAYERS=foo&STYLES=foo" `getFailsWith` MissingParameterError "SRS"
+    getMap100 "LAYERS=foo&STYLES=foo" `getFailsWith` MissingParameterError "SRS"
+    getMap130 "LAYERS=foo&STYLES=foo&CRS=EPSG:23030"
+      `getFailsWith` MissingParameterError "BBOX"
+    getMap130 "LAYERS=foo&STYLES=foo&CRS=EPSG:23030&BBOX=0,0,1,1"
+      `getFailsWith` MissingParameterError "WIDTH"
+    getMap130 "LAYERS=foo&STYLES=foo&CRS=EPSG:23030&BBOX=0,0,1,1&WIDTH=1"
+      `getFailsWith` MissingParameterError "HEIGHT"
+    getMap130
+      "LAYERS=foo&STYLES=foo&CRS=EPSG:23030&BBOX=0,0,1,1&WIDTH=1&HEIGHT=1"
+      `getFailsWith` MissingParameterError "FORMAT"
+
+  describe "with no optional parameters" $ do
+    getMap130Min "" `getSucceedsWith` mapReq
+
+  describe "with layers and styles length mismatch" $ do
+    getMap130 "LAYERS=foo,bar&STYLES=foo"
+      `getFailsWith` LayersStylesLengthMismatchError
+
+
 
 getFailsWith :: String -> OgcRequestError -> SpecWith Application
 getFailsWith url err =
--- src/Network/Wai/Ogc.hs
+++ src/Network/Wai/Ogc.hs
@@ -2,66 +2,175 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RecordWildCards #-}
 module Network.Wai.Ogc (
-    OgcRequest      (..)
-  , OgcRequestError (..)
-  , WmsVersion      (..)
-  , Service         (..)
+    OgcRequest       (..)
+  , OgcRequestError  (..)
+  , WmsVersion       (..)
+  , WmsMapExceptions (..)
+  , Service          (..)
+  , Bbox             (..)
+  , RgbColor         (..)
+  , Transparent      (..)
+  , Time             (..)
+  , TimeInterval     (..)
+  , TimeResolution   (..)
   , UpdateSequence
+
   , parseRequest
+
   , wmsCapabilitiesRequest
+  , wmsMapRequest
+
     -- * Re-exports
   , MIME.Type (..)
   , MIME.MIMEType (..)
   , MIME.MIMEParam (..)
+  , module SR
 ) where
 
-import           Control.Arrow ((***))
+import           Control.Applicative ((<|>))
+import           Control.Arrow (first)
 import qualified Codec.MIME.Parse as MIME
 import qualified Codec.MIME.Type as MIME
+import qualified Data.Attoparsec.ByteString.Char8 as AP
 import           Data.ByteString (ByteString)
 import           Data.CaseInsensitive (CI)
 import qualified Data.CaseInsensitive as CI
 import qualified Data.List as L
+import           Data.Scientific (Scientific)
 import           Data.String (IsString)
+import           Data.Text (Text)
 import           Data.Text.Encoding (decodeUtf8With)
 import           Data.Text.Encoding.Error (lenientDecode)
+import           Data.Time (UTCTime)
 import           Data.Typeable (Typeable)
+import           Data.Word (Word8)
+import           SpatialReference as SR
 import           Network.Wai (Request, queryString)
 
 --
 -- * Public types and functions
 --
 
+type Layer = Text
+type Style = Text
+
+data Bbox =
+  Bbox {
+    minx :: !Scientific
+  , miny :: !Scientific
+  , maxx :: !Scientific
+  , maxy :: !Scientific
+  } deriving Show
+
+data RgbColor =
+  RgbColor {
+    red   :: !Word8
+  , green :: !Word8
+  , blue  :: !Word8
+  } deriving Show
+
+data Transparent = Opaque | Transparent
+  deriving (Show, Eq, Ord, Enum, Bounded)
+
+
 data OgcRequest
   = WcsRequest
   | WfsRequest
   | WmsCapabilitiesRequest {
-      format         :: Maybe MIME.Type
-    , version        :: Maybe WmsVersion
-    , updateSequence :: Maybe UpdateSequence
+      wmsCapFormat         :: Maybe MIME.Type
+    , wmsCapVersion        :: Maybe WmsVersion
+    , wmsCapUpdateSequence :: Maybe UpdateSequence
+    }
+  | WmsMapRequest {
+      wmsMapVersion          :: WmsVersion
+    , wmsMapLayers           :: [(Layer, Style)]
+    , wmsMapCrs              :: SR.Crs
+    , wmsMapBbox             :: Bbox
+    , wmsMapWidth            :: Int
+    , wmsMapHeight           :: Int
+    , wmsMapFormat           :: MIME.Type
+    , wmsMapTransparent      :: Maybe Transparent
+    , wmsMapBackground       :: Maybe RgbColor
+    , wmsMapExceptions       :: Maybe WmsMapExceptions
+    , wmsMapTime             :: Maybe (Either Time TimeInterval)
+    , wmsMapElevation        :: Maybe Scientific
+    , wmsMapDimensions       :: [Dimension]
     }
   | WpsRequest
-  deriving (Show)
+  deriving Show
+
+data Time = Time UTCTime | Current
+  deriving Show
+
+data TimeInterval =
+  TimeInterval {
+    tiStart      :: Time
+  , tiEnd        :: Time
+  , tiResolution :: Maybe TimeResolution
+  } deriving Show
+
+data TimeResolution =
+  TimeResolution {
+    resYears   :: Maybe Int
+  , resMonths  :: Maybe Int
+  , resDays    :: Maybe Int
+  , resHours   :: Maybe Int
+  , resMinutes :: Maybe Int
+  , resSeconds :: Maybe Int
+  } deriving Show
+
+
+type DimensionName  = ByteString  -- ^ without the "DIM_" prefix
+type DimensionValue = ByteString  -- ^ unparsed since it is application dependant
+type Dimension      = (DimensionName, DimensionValue)
 
 wmsCapabilitiesRequest :: OgcRequest
 wmsCapabilitiesRequest = WmsCapabilitiesRequest Nothing Nothing Nothing
 
+wmsMapRequest
+  :: WmsVersion
+  -> [(Layer, Style)]
+  -> SR.Crs
+  -> Bbox
+  -> Int
+  -> Int
+  -> MIME.Type
+  -> OgcRequest
+wmsMapRequest version layers crs bbox width height format =
+  WmsMapRequest {
+    wmsMapVersion     = version
+  , wmsMapLayers      = layers
+  , wmsMapCrs         = crs
+  , wmsMapBbox        = bbox
+  , wmsMapWidth       = width
+  , wmsMapHeight      = height
+  , wmsMapFormat      = format
+  , wmsMapTransparent = Nothing
+  , wmsMapBackground  = Nothing
+  , wmsMapExceptions  = Nothing
+  , wmsMapTime        = Nothing
+  , wmsMapElevation   = Nothing
+  , wmsMapDimensions  = []
+  }
+
 data WmsVersion
-  = Wms111
+  = Wms100
+  | Wms111
   | Wms130
   deriving (Show, Eq, Ord, Enum, Bounded)
 
 data OgcRequestError
-  = UnknownServiceError      ByteString
-  | MissingParameterError    ByteString
-  | UnknownRequestError      Service ByteString
-  | UnknownVersionError      Service ByteString
-  | InvalidMimeError         ByteString
-  | EmptyUpdateSequenceError
+  = MissingParameterError           ByteString
+  | EmptyParameterError             ByteString
+  | InvalidParameterError           ByteString ByteString
+  | LayersStylesLengthMismatchError
   | NotImplementedError
   deriving (Show, Typeable)
 
+data WmsMapExceptions = WmsMapExcXml | WmsMapExcInImage | WmsMapExcBlank
+  deriving (Show, Eq, Ord, Enum, Bounded)
 
 newtype UpdateSequence = UpdateSequence ByteString
   deriving (Eq, Ord, Show, IsString)
@@ -88,72 +197,167 @@ data QueryRequest
   | GetFeatureInfo
   deriving (Show, Typeable)
 
-type ByteStringCI = CI ByteString
-
-type QueryCI = [(ByteStringCI, Maybe ByteStringCI)]
+type QueryCI = [(CI ByteString, Maybe ByteString)]
 
 data Service = WCS | WFS | WMS | WPS
   deriving Show
 
 reqService :: QueryCI -> Either OgcRequestError Service
-reqService query =
-  case L.lookup "SERVICE" query of
-    Just (Just "WCS")   -> Right WCS
-    Just (Just "WFS")   -> Right WFS
-    Just (Just "WMS")   -> Right WMS
-    Just (Just "WPS")   -> Right WPS
-    Just (Just service) -> Left (UnknownServiceError (CI.original service))
-    Just Nothing        -> Left (UnknownServiceError "")
-    Nothing             -> Left (MissingParameterError "SERVICE")
+reqService = mandatoryParameter "SERVICE" $
+  AP.choice [
+    AP.stringCI "WCS" *> pure WCS
+  , AP.stringCI "WFS" *> pure WFS
+  , AP.stringCI "WMS" *> pure WMS
+  , AP.stringCI "WPS" *> pure WPS
+  ]
 
 parseWmsRequest :: Request -> Either OgcRequestError OgcRequest
 parseWmsRequest req = do
-  request <- reqWmsRequest
+  version <- reqWmsVersion
+  request <- reqWmsRequest version
   case request of
-    GetCapabilities ->
-      WmsCapabilitiesRequest <$> reqFormat query
-                             <*> reqWmsVersion
-                             <*> reqUpdateSequence query
-    GetMap         -> undefined
-    GetFeatureInfo -> undefined
+    GetCapabilities -> parseWmsCapabilitiesRequest version
+    GetMap          -> parseWmsMapRequest version
+    GetFeatureInfo  -> undefined
   where
     query = queryStringCI req
-    reqWmsRequest =
-      case L.lookup "REQUEST" query of
-        Just (Just "GetCapabilities") -> Right GetCapabilities
-        Just (Just "GetMap")          -> Right GetMap
-        Just (Just "GetFeatureInfo")  -> Right GetFeatureInfo
-        Just (Just r)                 ->
-          Left (UnknownRequestError WMS (CI.original r))
-        Just Nothing                  -> Left (UnknownRequestError WMS "")
-        Nothing                       -> Left (MissingParameterError "REQUEST")
-    reqWmsVersion =
-      case L.lookup "VERSION" query of
-        Just (Just "1.1.1") -> Right (Just Wms111)
-        Just (Just "1.3.0") -> Right (Just Wms130)
-        Nothing             -> Right Nothing
-        Just (Just r)       -> Left (UnknownVersionError WMS (CI.original r))
-        Just Nothing        -> Left (UnknownVersionError WMS (""))
+
+    parseWmsCapabilitiesRequest wmsCapVersion = do
+      wmsCapFormat <- reqFormat query
+      wmsCapUpdateSequence <- reqUpdateSequence query
+      return WmsCapabilitiesRequest {..}
+
+    parseWmsMapRequest Nothing = Left (MissingParameterError "VERSION")
+    parseWmsMapRequest (Just wmsMapVersion) = do
+      wmsMapLayers <- reqLayers query
+      let crsKey = if wmsMapVersion == Wms130 then "CRS" else "SRS"
+      wmsMapCrs <- reqCrs crsKey query
+      wmsMapBbox <- reqBbox query
+      wmsMapWidth <- reqWidth query
+      wmsMapHeight <- reqHeight query
+      wmsMapFormat <- maybe (Left (MissingParameterError "FORMAT")) return
+                  =<< reqFormat query
+      wmsMapTransparent <- return Nothing -- FIXME
+      wmsMapBackground <- return Nothing -- FIXME
+      wmsMapExceptions <- return Nothing -- FIXME
+      wmsMapTime <- return Nothing -- FIXME
+      wmsMapElevation <- return Nothing -- FIXME
+      wmsMapDimensions <- return [] -- FIXME
+      return WmsMapRequest {..}
+
+    reqWmsRequest version =
+      mandatoryParameter "REQUEST"  parser query
+      where
+        parser = case version of
+          Just Wms100 -> wms100RequestParser
+          Just _      -> wmsRequestParser
+          Nothing     -> wmsRequestParser <|> wms100RequestParser
+        wms100RequestParser = AP.choice [
+            AP.stringCI "capabilities" *> pure GetCapabilities
+          , AP.stringCI "map"          *> pure GetMap
+          , AP.stringCI "feature_info" *> pure GetFeatureInfo
+          ]
+        wmsRequestParser = AP.choice [
+            AP.stringCI "GetCapabilities" *> pure GetCapabilities
+          , AP.stringCI "GetMap"          *> pure GetMap
+          , AP.stringCI "GetFeatureInfo"  *> pure GetFeatureInfo
+          ]
+
+    reqWmsVersion = maybe tryWmsTver (return . Just) =<< tryVersion
+      where
+        tryVersion = optionalParameter "VERSION"  parser query
+          where
+            parser = AP.choice [
+                "1.1.1"             *> pure Wms111
+              , ("1.3.0" <|> "1.3") *> pure Wms130
+              ]
+        tryWmsTver = optionalParameter "WMSTVER"  parser query
+          where
+            parser = ("1.0.0" <|> "1.0" <|> "1") *> pure Wms100
+
+reqLayers :: QueryCI -> Either OgcRequestError [(Layer,Style)]
+reqLayers query = do
+  layers <- mandatoryParameter "LAYERS" parser query
+  styles <- mandatoryParameter "STYLES" parser query
+  if length layers == length styles
+    then Right (zip layers styles)
+    else Left LayersStylesLengthMismatchError
+  where
+    parser = (lenientDecodeUtf8 <$> AP.takeWhile1 (/=','))
+              `AP.sepBy1` (AP.char ',')
+
+reqCrs :: CI ByteString -> QueryCI -> Either OgcRequestError SR.Crs
+reqCrs key = mandatoryParameter key (epsg <|> crs)
+  where
+    epsg, crs :: AP.Parser SR.Crs
+    crs  = undefined
+    epsg = maybe (fail "invalid epsg") return
+       =<< (SR.epsgCrs <$> (AP.stringCI "EPSG:" *> AP.decimal))
+
+
+reqBbox :: QueryCI -> Either OgcRequestError Bbox
+reqBbox = mandatoryParameter "BBOX" parser
+  where parser = Bbox <$> (AP.scientific <* AP.char ',')
+                      <*> (AP.scientific <* AP.char ',')
+                      <*> (AP.scientific <* AP.char ',')
+                      <*> AP.scientific
+
+reqWidth, reqHeight :: QueryCI -> Either OgcRequestError Int
+reqWidth  = mandatoryParameter "WIDTH" posInt
+reqHeight = mandatoryParameter "HEIGHT" posInt
 
 reqFormat :: QueryCI -> Either OgcRequestError (Maybe MIME.Type)
 reqFormat query =
   case L.lookup "FORMAT" query of
-    Just (Just (CI.original -> fmt))
+    Just (Just fmt)
       | Just mime <- parseMIME fmt -> Right (Just mime)
-      | otherwise                  -> Left  (InvalidMimeError fmt)
-    Just Nothing                   -> Left  (InvalidMimeError "")
+      | otherwise                  -> Left  (InvalidParameterError "FORMAT" fmt)
+    Just Nothing                   -> Left  (EmptyParameterError "FORMAT")
     Nothing                        -> Right Nothing
   where
-    parseMIME = MIME.parseMIMEType . decodeUtf8With lenientDecode
-
+    parseMIME = MIME.parseMIMEType . lenientDecodeUtf8
 
 reqUpdateSequence :: QueryCI -> Either OgcRequestError (Maybe UpdateSequence)
-reqUpdateSequence query =
-  case L.lookup "UPDATESEQUENCE" query of
-    Just (Just (CI.original -> useq)) -> Right (Just (UpdateSequence useq))
-    Just Nothing                      -> Left  EmptyUpdateSequenceError
-    Nothing                           -> Right Nothing
+reqUpdateSequence = optionalParameter
+  "UPDATESEQUENCE" (UpdateSequence <$> AP.takeWhile1 (const True))
+
+--
+-- * Utils
+--
+
+posInt :: AP.Parser Int
+posInt = do
+  n <- AP.decimal
+  if n==0 then fail "Negative number" else return n
+
+lenientDecodeUtf8 :: ByteString -> Text
+lenientDecodeUtf8 = decodeUtf8With lenientDecode
+
+mandatoryParameter
+  :: CI ByteString
+  -> AP.Parser a
+  -> QueryCI
+  -> Either OgcRequestError a
+mandatoryParameter key parser
+  = either Left (maybe (Left (MissingParameterError (CI.original key))) Right)
+  . optionalParameter key parser
+
+optionalParameter
+  :: CI ByteString
+  -> AP.Parser a
+  -> QueryCI
+  -> Either OgcRequestError (Maybe a)
+optionalParameter key parser query =
+  case L.lookup key query of
+    Just (Just "") -> Left (EmptyParameterError (CI.original key))
+    Just (Just s) ->
+      either (const (Left (InvalidParameterError (CI.original key) s)))
+             (Right . Just)
+             (AP.parseOnly (parser <* AP.endOfInput) s)
+    Just Nothing -> Left (EmptyParameterError (CI.original key))
+    Nothing      -> Right Nothing
+
 
 -- | Parses a query string into case-insensitive elements
 queryStringCI :: Request -> QueryCI
-queryStringCI = map (CI.mk *** fmap CI.mk) . queryString
+queryStringCI = map (first CI.mk) . queryString
